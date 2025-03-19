c24ada13362c534c56ccc9b28879208c6040857d
AlexRowshan <154560572+AlexRowshan@users.noreply.github.com>
2025-03-19 15:16:49 -0700

    Update Constants.swift

--- s2s/Model/Services/Constants.swift
+++ s2s/Model/Services/Constants.swift
@@ -9,5 +9,5 @@ import Foundation
 
 enum Constants {
     static let authToken : String? = "Bearer gxye69"
-    static let openAIAPIKey : String = "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"
+    static let openAIAPIKey : String = "_hidden"
 }
